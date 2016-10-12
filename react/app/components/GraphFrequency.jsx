2869986c4a374f637c66d83cd5166ed8e123f5e2
Casey Hutchinson <hutster@gmail.com>
2016-10-12 02:34:42 -0400

    Added graph placeholders

--- react/app/components/GraphFrequency.jsx
+++ react/app/components/GraphFrequency.jsx
@@ -2,9 +2,14 @@ var React = require('react');
 
 var GraphFrequency = React.createClass({
   render: function() {
+    var canvasStyle = {
+      border: '1px solid #000000'
+    };
     return (
       <div>
-        <p>Rendered GraphFrequency</p>
+        <p>Rendered GraphWave</p>
+          <canvas id="GraphWaveCanvas" width="200" height="100" style={canvasStyle}>
+          </canvas>
       </div>
     );
   }
--- react/app/components/GraphWave.jsx
+++ react/app/components/GraphWave.jsx
@@ -2,9 +2,14 @@ var React = require('react');
 
 var GraphWave = React.createClass({
   render: function() {
+    var canvasStyle = {
+      border: '1px solid #000000'
+    };
     return (
       <div>
         <p>Rendered GraphWave</p>
+          <canvas id="GraphWaveCanvas" width="200" height="100" style={canvasStyle}>
+          </canvas>
       </div>
     );
   }
