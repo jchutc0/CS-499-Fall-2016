1e5809344f4d9d7ad89e044b43048777989b40c1
Casey Hutchinson <hutchster@gmail.com>
2016-10-12 11:42:45 -0400

    Added additional dummy classes and tutorial (try 2)

---
+++ react/app/components/AudioOut.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var AudioOut = React.createClass({
+  render: function() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+});
+
+module.exports = AudioOut;
