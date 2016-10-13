5e5520858e57b999d79a7b54708b7d039aaede7f
Casey Hutchinson <hutchster@gmail.com>
2016-10-13 10:53:34 -0400

    Added gain to frequency form

--- react/app/components/FormFrequency.jsx
+++ react/app/components/FormFrequency.jsx
@@ -7,33 +7,95 @@ var FormFrequency = React.createClass({
 
   getInitialState: function() {
     return {
-      play: 0
+      playing: false
     };
   },
 
-  toggleUserFrequency: function(e) {
+  playUserFrequency: function(e) {
     e.preventDefault();
+    this.setState({
+      playing: true
+    });
     var frequencyObject = {
       frequency1: this.refs.frequency1.value,
-      gain1: 0.1,
+      gain1: this.refs.gain1.value,
       frequency2: this.refs.frequency2.value,
-      gain2: 0.1
+      gain2: this.refs.gain2.value
     };
     this.props.playFrequency(frequencyObject);
+  },
+
+  stopUserFrequency: function(e) {
+    e.preventDefault();
+    this.setState({
+      playing: false
+    });
+    var frequencyObject = {
+      frequency1: 0,
+      gain1: 0,
+      frequency2: 0,
+      gain2: 0
+    };
+    this.props.playFrequency(frequencyObject);
+  },
+
+  clearForm: function(e) {
+    this.stopUserFrequency(e);
+    this.refs.frequency1.value = '';
+    this.refs.gain1.value = '';
+    this.refs.frequency2.value = '';
+    this.refs.gain2.value = '';
+  },
+
+  renderPlayButton: function() {
+    if(this.state.playing) {
+      return (
+        <button className='secondary button' id='stopSound' onClick={this.stopUserFrequency}>Stop Frequency</button>
+      );
+    } else {
+      return (
+        <button className='button' id='startSound' onClick={this.playUserFrequency}>Play Frequency</button>
+      );
+    }
   },
 
   render: function() {
     return (
       <div>
-        <fieldset>
-        <legend>Sound Info</legend>
-        <label htmlFor='frequency1' >Frequency 1:</label>
-        <input type='number' ref='frequency1' name='frequency1' id='frequency1' maxLength="5" />
-        <label htmlFor='frequency2' >Frequency 2:</label>
-        <input type='number' ref='frequency2' name='frequency2' id='frequency2' maxLength="5" />
-        <button className='button' id='toggleSound' onClick={this.toggleUserFrequency}>Play Frequency</button><br/>
-        <br/>
+        <fieldset className="frequency-fieldset">
+          <legend>Tone 1</legend>
+          <div className="row">
+            <div className="columns small-12 medium-6">
+              <label htmlFor='frequency1'>Frequency:</label>
+              <input type='number' ref='frequency1' name='frequency1' id='frequency1' maxLength="5" />
+            </div>
+            <div className="columns small-12 medium-6">
+              <label htmlFor='gain1' >Gain:</label>
+              <input type='number' ref='gain1' name='gain1' id='gain1' maxLength="5" />
+            </div>
+          </div>
         </fieldset>
+        <fieldset className="frequency-fieldset">
+          <legend>Tone 2</legend>
+            <div className="row">
+              <div className="columns small-12 medium-6">
+                <label htmlFor='frequency2'>Frequency:</label>
+                <input type='number' ref='frequency2' name='frequency2' id='frequency2' maxLength="5" />
+              </div>
+              <div className="columns small-12 medium-6">
+                <label htmlFor='gain2' >Gain:</label>
+                <input type='number' ref='gain2' name='gain2' id='gain2' maxLength="5" />
+              </div>
+          </div>
+        </fieldset>
+        <div className='row'>
+          <div className='columns small-12 medium-6'>
+            {this.renderPlayButton()}
+          </div>
+          <div className='columns small-12 medium-6'>
+            <button className='button' id='clearForm' onClick={this.clearForm}>Clear Form</button><br/>
+          </div>
+        </div>
       </div>
     );
   }
