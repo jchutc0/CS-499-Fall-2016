817d3332449185b23cba83a53e0eec084fd9ea9d
Casey Hutchinson <hutchster@gmail.com>
2016-10-25 15:41:02 -0400

    commented FormWhiteNoise

--- react/app/components/FormWhiteNoise.jsx
+++ react/app/components/FormWhiteNoise.jsx
@@ -1,57 +1,104 @@
+/*****
+FormWhiteNoise class
+
+The purpose of this class is to display the form for the white noise class
+*****/
+
 // Require the React framework
 var React = require('react');
 
+// Create the FormWhiteNoise class
 var FormWhiteNoise = React.createClass({
+
+  // require the playWhiteNoise function as a passed property
   propTypes: {
     playWhiteNoise: React.PropTypes.func.isRequired
-  },
+  },      // propTypes
 
+  /*
+	getInitialState function
+
+	Set up initial playing state to false (not playing)
+	*/
   getInitialState: function() {
     return {
       playing: false
     };
   },
 
+  /*
+  playWhiteNoise function
+
+  Invoked from a stop noise button press
+
+  updates playing state, sends frequencyObj to playWhiteNoise prop
+  */
   playWhiteNoise: function(e) {
+    // prevent page from reloading
     e.preventDefault();
+
+    // set playing state
     this.setState({
       playing: true
-    });
+    });       // setState
 
     var whiteNoiseGain = (this.refs.whiteNoiseGain.value % 100) / 100.0;
 
-    var frequencyObject = {
+    this.props.playWhiteNoise({
       whiteNoise: whiteNoiseGain
-    };
-    this.props.playWhiteNoise(frequencyObject);
-  },
+    });       // playWhiteNoise call
+  },          // playWhiteNoise function
 
+  /*
+  stopWhiteNoise function
+
+  Invoked from a stop noise button press
+
+  updates playing state, sends empty frequencyObj to playWhiteNoise prop
+  */
   stopWhiteNoise: function(e) {
+    // prevent page from reloading
     e.preventDefault();
+
+    // set playing state
     this.setState({
       playing: false
-    });
-    var frequencyObject = {
+    });     // setState
+
+    // send empty frequency to playWhiteNoise prop
+    this.props.playWhiteNoise({
       frequency1: 0,
       gain1: 0,
       frequency2: 0,
       gain2: 0
-    };
-    this.props.playWhiteNoise(frequencyObject);
-  },
+    });     // playWhiteNoise call
+  },        // stopWhiteNoise function
 
+  /*
+  renderPlayWhiteNoiseButton function
+
+  renders the button to play or stop white noise, depending on the playing
+  state
+  */
   renderPlayWhiteNoiseButton: function() {
     if(this.state.playing) {
       return (
-        <button className='secondary button' id='stopSound' onClick={this.stopWhiteNoise}>Stop White Noise</button>
-      );
+        <button className='secondary button' id='stopSound'
+          onClick={this.stopWhiteNoise}>Stop White Noise</button>
+      );        // return value
     } else {
       return (
-        <button className='button' id='startSound' onClick={this.playWhiteNoise}>Play White Noise</button>
-      );
-    }
-  },
+        <button className='button' id='startSound'
+          onClick={this.playWhiteNoise}>Play White Noise</button>
+      );        // return value
+    }           // if..else state playing set
+  },            // renderPlayWhiteNoiseButton
 
+  /*
+  render function
+
+  renders the component to the web browser -- the default entry point
+  */
   render: function() {
     return (
       <div>
@@ -60,7 +107,8 @@ var FormWhiteNoise = React.createClass({
           <div className="row">
             <div className="columns small-12 medium-6">
               <label htmlFor='whiteNoiseGain' >Gain:</label>
-              <input type='number' ref='whiteNoiseGain' name='whiteNoiseGain' id='whiteNoiseGain' maxLength="5" />
+              <input type='number' ref='whiteNoiseGain' name='whiteNoiseGain'
+                id='whiteNoiseGain' maxLength="5" />
             </div>
           </div>
         </fieldset>
@@ -70,8 +118,9 @@ var FormWhiteNoise = React.createClass({
           </div>
         </div>
       </div>
-    );
-  }
-});
+    );        // return value
+  }           // render function
+});           // FormWhiteNoise class
 
+// export FormWhiteNoise for other modules to use
 module.exports = FormWhiteNoise;
