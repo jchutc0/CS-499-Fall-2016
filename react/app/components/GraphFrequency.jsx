d032f8b5e994b1b3723dc78d48a9b6adb5e7410a
Casey Hutchinson <hutchster@gmail.com>
2016-10-25 16:42:48 -0400

    Commented Graph components

--- react/app/components/GraphFrequency.jsx
+++ react/app/components/GraphFrequency.jsx
@@ -1,19 +1,35 @@
+/*****
+GraphFrequency class
+
+The purpose of this class is to display graph for the frequency from the FFT
+*****/
+
 // Require the React framework
 var React = require('react');
 
+// Create the GraphFrequency class
 var GraphFrequency = React.createClass({
+
+  /*
+  render function
+
+  renders the component to the web browser -- the default entry point
+  */
   render: function() {
+    // set up style for canvas
     var canvasStyle = {
       border: '1px solid #000000'
     };
+
     return (
       <div>
         <p>Rendered GraphWave</p>
           <canvas id="GraphWaveCanvas" width="400" height="200" style={canvasStyle}>
           </canvas>
       </div>
-    );
-  }
-});
+    );        // return value
+  }           // render function
+});           // GraphFrequency class
 
+// export GraphFrequency for other modules to use
 module.exports = GraphFrequency;
--- react/app/components/GraphWave.jsx
+++ react/app/components/GraphWave.jsx
@@ -1,20 +1,35 @@
+/*****
+GraphWave class
+
+The purpose of this class is to display graph for the audio wave
+*****/
+
 // Require the React framework
 var React = require('react');
 
+// Create the GraphWave class
 var GraphWave = React.createClass({
+
+  /*
+  render function
+
+  renders the component to the web browser -- the default entry point
+  */
   render: function() {
+    // set up style for canvas
     var canvasStyle = {
       border: '1px solid #000000'
     };
+
     return (
       <div>
         <p>Rendered GraphWave</p>
           <canvas id="GraphWaveCanvas" width="400" height="200" style={canvasStyle}>
           </canvas>
       </div>
-    );
-  }
-});
+    );        // return value
+  }           // render function
+});           // GraphWave class
 
-
+// export GraphWave for other modules to use
 module.exports = GraphWave;
