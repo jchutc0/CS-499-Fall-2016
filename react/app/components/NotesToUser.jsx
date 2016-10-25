60e25cd2dc54ecd6d405acf8fdd09f51b1199905
Casey Hutchinson <hutchster@gmail.com>
2016-10-25 16:48:22 -0400

    commented NotesToUser component

--- react/app/components/NotesToUser.jsx
+++ react/app/components/NotesToUser.jsx
@@ -1,14 +1,32 @@
+/*****
+FormWhiteNoise class
+
+The purpose of this class is to display the form for the white noise class
+*****/
+
 // Require the React framework
 var React = require('react');
 
+// Create the NotesToUser class
 var NotesToUser = React.createClass({
 
+  /*
+  getDefaultProps function
+
+  Sets default message property -- changed if the calling component passes
+  another prop
+  */
   getDefaultProps: function() {
     return {
       message: 'This is the default message.'
-    };
-  },
+    };        // default props object
+  },          // getDefaultProps function
 
+  /*
+  render function
+
+  renders the component to the web browser -- the default entry point
+  */
   render: function() {
     return (
       <div>
@@ -19,5 +37,5 @@ var NotesToUser = React.createClass({
   }
 });
 
-
+// export NotesToUser for other modules to use
 module.exports = NotesToUser;
