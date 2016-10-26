0025a728488699d3c9d36623572f3929267a2487
Casey Hutchinson <hutchster@gmail.com>
2016-10-26 16:28:37 -0400

    Got GraphWave working in a basic sense

--- react/app/components/Graph.jsx
+++ react/app/components/Graph.jsx
@@ -16,15 +16,43 @@ var GraphFrequency = require('GraphFrequency');
 var Graph = React.createClass({
 
   /*
+  generateWaveform
+
+  Generates an array of values for a cosine waveform based on passed parameters
+  Takes: size - desired output array size; count - number of desired repeitions
+  */
+  generateWaveform: function(size, count) {
+    // make sure we have useful information
+    if(
+      (size === undefined) || (size < 1) || (!Number.isInteger(size)) ||
+      (count === undefined) || (count < 1) || (!Number.isInteger(count))
+    ) {
+      return [];
+    }
+
+    // create array to return
+    var returnArray = new Array(size);
+
+    // fill array with wave values
+    for(var i = 0; i < size; i++) {
+      returnArray[i] = Math.cos(count * 2 * Math.PI / size * i);
+    }
+    return returnArray;
+  },        // generateWaveform function
+
+  /*
   render function
 
   renders the component to the web browser -- the default entry point
   */
   render: function() {
+    // create generic data to send to GraphWave
+    var graphWaveArray = this.generateWaveform(1024, 1, 11);
+
     return (
       <div>
         <p>Rendered Graph</p>
-        <GraphWave />
+        <GraphWave data={graphWaveArray}/>
         <GraphFrequency />
       </div>
     );        // return value
--- react/app/components/GraphWave.jsx
+++ react/app/components/GraphWave.jsx
@@ -13,6 +13,13 @@ var GraphWave = React.createClass({
   // declare width and height constants for easy changes
   width: 400,
   height: 200,
+  // declare the usable percentage of the graph (to make it pretty)
+  usable: 75,
+
+  // component takes a required array of numbers
+  propTypes: {
+    data   : React.PropTypes.array.isRequired
+  },    // propTypes
 
   /*
   componentDidMount function
@@ -21,6 +28,8 @@ var GraphWave = React.createClass({
   Draws the graph
   */
   componentDidMount: function() {
+    var data = this.props.data;
+
     // defines the canvas and the draw context for the graph
     var canvas = this.refs.waveGraphCanvas;
     var drawContext = canvas.getContext('2d');
@@ -29,7 +38,77 @@ var GraphWave = React.createClass({
     drawContext.moveTo(0, this.height / 2);
     drawContext.lineTo(this.width, this.height / 2);
     drawContext.stroke();
-  },
+
+    // now generate arrays of horizontal and vertical coordinates
+    var horizontalCoords = this.generateHorizontalCoords(data.length);
+    var verticalCoords = this.generateVerticalCoords(data);
+
+    for(var i = 0; i < data.length; i++) {
+      if(i !== 0) {
+        drawContext.lineTo(horizontalCoords[i], verticalCoords[i]);
+      } else {
+        drawContext.moveTo(horizontalCoords[i], verticalCoords[i]);
+      }   // if..else for i being the first element
+    }     // array iteration for loop
+
+    drawContext.stroke();
+  },      // componentDidMount function
+
+  /*
+  generateVerticalCoords function
+
+  Takes an array of numbers between -1 and 1 and generates an array of values to
+  map those points onto a graph having dimensions this.width by this.height
+  using this.usable vertical percent of the graph
+  */
+  generateVerticalCoords: function(data) {
+    var {height, usable} = this;
+
+    // to fit the graph right, we need to rescale the numbers around 0 and then
+    //   shift them up so they're at the center of the graph
+    var scalingFactor = height / 2 * usable / 100;
+    var shiftingFactor = height / 2;
+
+    // set up array to return values
+    var returnArray = new Array(data.length);
+
+    for(var i = 0; i < data.length; i++) {
+      returnArray[i] = Math.floor(data[i] * scalingFactor + shiftingFactor);
+    }
+
+    return returnArray;
+  },      // generateVerticalCoords function
+
+  /*
+  generateHorizontalCoords function
+
+  Takes the length of an array and generates an array of values to map those
+  points onto a graph having dimensions this.width by this.height
+  */
+  generateHorizontalCoords: function(length) {
+    var {width, usable} = this;
+
+    // we should never get < 0 element arrays, but just in case
+    if (length <= 0) {
+      return [];
+    } else if (length == 1) {
+      // prevent a divide by 0 error in case of 1 element arrays
+      return [0];
+    }
+
+    // to scale the data to the graph size
+    var scalingFactor = width / (length - 1);
+
+    // set up array to return values
+    var returnArray = new Array(length);
+
+    for(var i = 0; i < length; i++) {
+      returnArray[i] = Math.floor(i * scalingFactor);
+    }
+
+    return returnArray;
+  },      // generateVerticalCoords function
+
 
   /*
   render function
@@ -45,9 +124,9 @@ var GraphWave = React.createClass({
     return (
       <div>
         <p>Rendered GraphWave</p>
-          <canvas id="GraphWaveCanvas" width={this.width}
-            height={this.height} ref='waveGraphCanvas' style={canvasStyle}>
-          </canvas>
+        <canvas id="GraphWaveCanvas" width={this.width}
+          height={this.height} ref='waveGraphCanvas' style={canvasStyle}>
+        </canvas>
       </div>
     );        // return value
   }           // render function
