abe8a5a194fc2260d1b1358fc27bcac9ef64475c
Casey Hutchinson <hutchster@gmail.com>
2016-11-28 17:47:41 -0500

    implemented circular arrays for Shephard tone

--- react/app/components/FormShephards.jsx
+++ react/app/components/FormShephards.jsx
@@ -36,78 +36,98 @@ var FormShephards = React.createClass({
   variance: 1,
   meanFreq: 440,
 
-  // maxFrequency, the highest frequency level we'll use
-  maxFrequency: 17000,
+  // numberOfSamples, the number of samples to play
+  //   with 10 samples, high values go from 8371.2 (C) to 15,805.44 (B)
+  // 10 was chosen since maximum number of samples all musical pitches can
+  //   represent and still be heard by the human ear
+  numberOfSamples: 10,
+
+  // // maxFrequency, the highest frequency level we'll use
+  // maxFrequency: 17000,
 
   getInitialState: function() {
     return {
-      lowTone: 0
+      lowTone: 0,
+      arrayBase: 0
     };
   },
 
-  playShephards: function(lowTone) {
-    var frequencyArray = [];
-    var gainArray;
+  playShephards: function(lowTone, arrayBase) {
+
+    var startTone = this.lowTones[lowTone].frequency;
+    var arraySize = this.numberOfSamples;
+    var frequencyArray = new Array(arraySize);
+    var gainArray = new Array(arraySize);
+
     var variance = this.variance;
-    var mean = Math.log(this.meanFreq);
-    for (
-      var frequency = this.lowTones[lowTone].frequency;
-      frequency < this.maxFrequency;
-      frequency = frequency * 2
-    ) {
-      frequencyArray.push(frequency);
+    var mean = Math.log2(this.meanFreq);
+    var frequency = startTone;
+    for(var i = 0; i < arraySize; i++) {
+      frequencyArray[(i+arrayBase+arraySize) % arraySize] = frequency;
+      frequency = frequency * 2;
     }
-    gainArray = new Array(frequencyArray.length);
-    for(var i = 0; i < frequencyArray.length; i++) {
+    for(var i = 0; i < arraySize; i++) {
       gainArray[i] = (
-        Math.exp(Math.pow((Math.log(frequencyArray[i]) - mean), 2)
-       / (variance * -2)));
-    }
-    this.props.playFrequency(frequencyArray, gainArray);
-  },
+        Math.exp(Math.pow((Math.log2(frequencyArray[i]) - mean), 2)
+        / (variance * -2)));
+      }
+      this.props.playFrequency(frequencyArray, gainArray);
+    },
 
-  handleSoundUpDown: function(e) {
-    e.preventDefault();
-    var newTone = (this.state.lowTone + 1) % this.lowTones.length;
-    this.setState({
-      lowTone: newTone
-    });
-    this.playShephards(newTone);
-  },
+    handleSoundUp: function(e) {
+      e.preventDefault();
+      var arraySize = this.lowTones.length;
+      var newTone = (this.state.lowTone + 1) % arraySize;
+      var newArrayBase = this.state.arrayBase;
+      if(newTone < this.state.lowTone) {
+        newArrayBase = (newArrayBase - 1) % arraySize;
+      }
+      this.setState({
+        lowTone: newTone,
+        arrayBase: newArrayBase
+      });
+      this.playShephards(newTone, newArrayBase);
+    },
 
-  handleSoundDownDown: function(e) {
-    e.preventDefault();
-    var newTone = (this.state.lowTone + this.lowTones.length - 1) % this.lowTones.length;
-    this.setState({
-      lowTone: newTone
-    });
-    this.playShephards(newTone);
-  },
+    handleSoundDown: function(e) {
+      e.preventDefault();
+      var arraySize = this.lowTones.length;
+      var newTone = (this.state.lowTone + arraySize - 1) % arraySize;
+      var newArrayBase = this.state.arrayBase;
+      if(newTone > this.state.lowTone) {
+        newArrayBase = (newArrayBase + 1) % arraySize;
+      }
+      this.setState({
+        lowTone: newTone,
+        arrayBase: newArrayBase
+      });
+      this.playShephards(newTone, newArrayBase);
+    },
 
-  handleSoundButtonUp: function(e) {
-    e.preventDefault();
-    this.props.playFrequency([], []);
-  },
+    handleStopSound: function(e) {
+      e.preventDefault();
+      this.props.playFrequency([], []);
+    },
 
-  /*
-  render function
+    /*
+    render function
 
-  renders the component to the web browser -- the default entry point
-  */
-  render: function() {
-    return (
-      <div>
-        <button type='button' className='expanded button' ref='stopSound'
-          onMouseDown={this.handleSoundUpDown}
-          onMouseUp={this.handleSoundButtonUp}>Sound Up</button>
-        <button type='button' className='expanded button' ref='stopSound'
-          onMouseDown={this.handleSoundDownDown}
-          onMouseUp={this.handleSoundButtonUp}>Sound Down</button>
-        <p>Pitch played: {this.lowTones[this.state.lowTone].pitch}</p>
-      </div>
-    );        // return value
-  }           // render function
-});           // FormShephards class
+    renders the component to the web browser -- the default entry point
+    */
+    render: function() {
+      return (
+        <div>
+          <button type='button' className='expanded button' ref='soundUp'
+            onClick={this.handleSoundUp}>Sound Up</button>
+          <button type='button' className='expanded button' ref='soundDown'
+            onClick={this.handleSoundDown}>Sound Down</button>
+          <button type='button' className='expanded button' ref='stopSound'
+            onClick={this.handleStopSound}>Stop Sound</button>
+          <p>Curent Pitch: {this.lowTones[this.state.lowTone].pitch}</p>
+        </div>
+      );        // return value
+    }           // render function
+  });           // FormShephards class
 
-// export FormShephards for other modules to use
-module.exports = FormShephards;
+  // export FormShephards for other modules to use
+  module.exports = FormShephards;
